596a262521e3cdd72e5d1fab1bafebb30fe8a27e
Lucas Aleixo da Silva <lucas.silva@2rpnet.com>
2024-01-14 11:52:16 -0300

    Fist commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template
+import os
+import requests, json
+
+wpp_url = os.environ["WHATSAPP_API_URL"]
+chat_id = os.environ["CHAT_ID"]
+
+def send_notification(alert):
+    content = f"⚠️ {alert['name']}\n🚨 {alert['status']}\n🌐 {alert['dashboard']}"
+    payload = json.dumps({
+        "chatId": chat_id,
+        "contentType": "string",
+        "content": content
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", wpp_url, headers=headers, data=payload, verify=False)
+    if response.status_code == 200:
+        print("> whatsapp message sent")
+    else:
+        print(f"Error {response.status_code} sending message\n")
+        print(response.text)
+
+app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
+
+@app.route('/', methods=['POST'])
+def webhook():
+    alert = {}
+    data = request.json
+    alert["name"] = data['alerts'][0]['labels']['alertname']
+    alert["status"] = data['alerts'][0]['status']
+    alert["dashboard"] = data['alerts'][0]['dashboardURL']
+    print(f"> received alert")
+    print(alert)
+    try:
+        send_notification(alert)
+        return "notification sent"
+    except Exception as e:
+        print(f"> error, {e}")
+    
+
+app.run(host="0.0.0.0", port=5000)
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Grafana2Whatsapp
+This code was made to receive a webhook from grafana and send a post to
+whatsapp api to notify in a group chat.
